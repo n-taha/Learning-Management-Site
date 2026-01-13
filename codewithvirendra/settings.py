59f85b257b9012fe5758a194f64283545a175d0e
n.taha <mubtasimtaha@gmail.com>
2026-01-13 16:40:15 +0600

    update

--- codewithvirendra/settings.py
+++ codewithvirendra/settings.py
@@ -77,11 +77,21 @@ WSGI_APPLICATION = 'codewithvirendra.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=config('DATABASE_URL')
+#     )
+# }
+
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
+    'default': dj_database_url.parse(
+        "postgresql://postgres_gkrq_user:MUhpM90zirTUTqbUAQWQpCwnXvI5zOZD@dpg-d5j1ltili9vc73ami1dg-a/postgres_gkrq",
+        conn_max_age=600,
+        ssl_require=True
     )
 }
+
 
 # DATABASES = {
 #     'default': {
